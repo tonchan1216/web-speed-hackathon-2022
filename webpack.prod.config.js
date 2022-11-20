7230ccf0137fb3ab857b14e0456c26f2da42786b
tonchan1216 <cleartone1216@gmail.com>
2022-11-20 16:50:51 +0900

    babel loadash

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -17,9 +17,8 @@ const DIST_PUBLIC = abs("./dist/public");
 /** @type {Array<import('webpack').Configuration>} */
 module.exports = [
   {
-    devtool: "inline-source-map",
     entry: path.join(SRC_ROOT, "client/index.jsx"),
-    mode: "development",
+    mode: "production",
     module: {
       rules: [
         {
@@ -80,7 +79,6 @@ module.exports = [
     target: "web"  
   },
   {
-    devtool: "inline-source-map",
     entry: path.join(SRC_ROOT, "server/index.js"),
     externals: [nodeExternals()],
     mode: "development",
