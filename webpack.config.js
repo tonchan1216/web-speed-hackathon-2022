d3613e5743fe25ef07efe0042ca96d42df1b2f8b
tonchan1216 <cleartone1216@gmail.com>
2022-11-22 09:33:24 +0900

    split config

--- webpack.config.js
+++ webpack.config.js
@@ -1,124 +1,16 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const path = require("path");
+const { merge } = require("webpack-merge");
 
-const CopyPlugin = require("copy-webpack-plugin");
-const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
-const nodeExternals = require("webpack-node-externals");
+const common = require("./webpack.common.js");
 
-function abs(...args) {
-  return path.join(__dirname, ...args);
-}
+const clientConfig = common.findIndex(config => config.name === "client");
+const serverConfig = common.findIndex(config => config.name === "server");
 
-const SRC_ROOT = abs("./src");
-const PUBLIC_ROOT = abs("./public");
-const DIST_ROOT = abs("./dist");
-const DIST_PUBLIC = abs("./dist/public");
-
-/** @type {Array<import('webpack').Configuration>} */
 module.exports = [
-  {
-    entry: {
-      main: path.join(SRC_ROOT, "client/index.jsx"),
-      polyfill: ['core-js/stable', 'regenerator-runtime/runtime'],
-    },
+  merge(common[clientConfig], {
     mode: "production",
-    module: {
-      rules: [
-        {
-          resourceQuery: (value) => {
-            const query = new URLSearchParams(value);
-            return query.has("raw");
-          },
-          type: "asset/source",
-        },
-        {
-          exclude: /[\\/]esm[\\/]/,
-          test: /\.jsx?$/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              plugins: ['lodash'],
-              presets: [
-                [
-                  "@babel/preset-env",
-                  {
-                    corejs: 3,
-                    modules: "cjs",
-                    spec: true,
-                    targets: "defaults",
-                    useBuiltIns: 'usage'
-                  },
-                ],
-                "@babel/preset-react",
-              ],
-            },
-          },
-        },
-      ],
-    },
-    name: "client",
-    optimization: {
-      splitChunks: {
-        cacheGroups: {
-          vendors: {
-            chunks: 'initial',
-            name: 'vendors',
-            priority: 0,
-          },
-        },
-        chunks: 'initial'
-      },
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: DIST_PUBLIC,
-    },
-    plugins: [
-      new CopyPlugin({
-        patterns: [{ from: PUBLIC_ROOT, to: DIST_PUBLIC }],
-      }),
-      new LodashModuleReplacementPlugin()
-    ],
-    resolve: {
-      extensions: [".js", ".jsx"],
-    },
-    target: "web"  
-  },
-  {
-    entry: path.join(SRC_ROOT, "server/index.js"),
-    externals: [nodeExternals()],
+  }), 
+  merge(common[serverConfig], {
     mode: "development",
-    module: {
-      rules: [
-        {
-          exclude: /node_modules/,
-          test: /\.(js|mjs|jsx)$/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              presets: [
-                [
-                  "@babel/preset-env",
-                  {
-                    modules: "cjs",
-                    spec: true,
-                  },
-                ],
-                "@babel/preset-react",
-              ],
-            },
-          },
-        },
-      ],
-    },
-    name: "server",
-    output: {
-      filename: "server.js",
-      path: DIST_ROOT,
-    },
-    resolve: {
-      extensions: [".mjs", ".js", ".jsx"],
-    },
-    target: "node",
-  },
+  })
 ];
