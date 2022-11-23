e7f15eecb8d7a73b91dce626285b12a77e5f8299
tonchan1216 <cleartone1216@gmail.com>
2022-11-23 13:18:22 +0900

    delete

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,15 @@ const { merge } = require("webpack-merge");
 
 const common = require("./webpack.common.js");
 
-const clientConfig = common.findIndex(config => config.name === "client");
-const serverConfig = common.findIndex(config => config.name === "server");
+const clientConfig = common.findIndex((config) => config.name === "client");
+const serverConfig = common.findIndex((config) => config.name === "server");
 
 module.exports = [
   merge(common[clientConfig], {
+    devtool: "inline-source-map",
     mode: "production",
-  }), 
+  }),
   merge(common[serverConfig], {
     mode: "development",
-  })
+  }),
 ];
